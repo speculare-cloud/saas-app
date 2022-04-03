2a6474e0942d64e9f4545217009e16892bc8453c
Martichou <m@rtin.fyi>
2022-04-03 21:23:38 +0200

    logged: test a new way to check if logged

--- src/main.js
+++ src/main.js
@@ -8,26 +8,29 @@ import '@/assets/app.css'
 
 const app = createApp(App)
 
-// Check if we're logged (maybe check in another scope? does this block??)
-axios.get("https://auth.speculare.cloud/api/whoami", { withCredentials: true })
-	.then(() => {
-		store.commit({
-			type: 'setLogged',
-			isLogged: true
-		});
-	}).catch(() => {
-		store.commit({
-			type: 'setLogged',
-			isLogged: false
-		});
-	});
-
 // Enforce auth requirement for the views
-router.beforeEach((toRoute, _fromRoute, next) => {
-	// Check if the page need auth (and if so and logged out, redirect)
-	if (toRoute.meta.requireAuth && !store.state.isLogged) next({ name: 'Login' })
-	window.document.title = toRoute.name + " - Speculare Console";
-	next();
+router.beforeEach(async(toRoute, _fromRoute, next) => {
+    if (toRoute.meta.requireAuth && !store.state.isLogged) {
+        next({ name: 'Login' });
+    } else if (!toRoute.meta.requireAuth) {
+        await axios.get("https://auth.speculare.cloud/api/whoami", { withCredentials: true })
+            .then(() => {
+                store.commit({
+                    type: 'setLogged',
+                    isLogged: true
+                });
+                next({ name: 'Home' });
+            }).catch(() => {
+                store.commit({
+                    type: 'setLogged',
+                    isLogged: false
+                });
+                next();
+            });
+    } else {
+        window.document.title = toRoute.name + " - Speculare Console";
+        next();
+    }
 })
 
 app.use(router)
