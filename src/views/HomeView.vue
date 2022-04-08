464714f87fbc4d7036d93343a755d60894ac5ee2
Martichou <m@rtin.fyi>
2022-04-08 19:19:56 +0200

    home: dummy new key

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
 	<div>
 		<p>Hello World</p>
+
+		<button @click="generateKey" class="mb-2 flex w-full justify-center items-center bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 text-white rounded-lg px-4 py-3 mt-6">
+			<span>Generate new key</span>
+		</button>
+
 		<button @click="logout" class="mb-2 flex w-full justify-center items-center bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 text-white rounded-lg px-4 py-3 mt-6">
 			<span>Logout</span>
 		</button>
@@ -22,6 +27,14 @@ export default {
 						isLogged: false
 					});
 					this.$router.replace({ name: 'Login' });
+				}).catch((err) => {
+					console.log(err);
+				});
+		},
+		generateKey: async function() {
+			await axios.post("https://auth.speculare.cloud/api/key", { withCredentials: true })
+				.then((resp) => {
+					console.log("New key: ", resp);
 				}).catch((err) => {
 					console.log(err);
 				});
