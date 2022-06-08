0030350b9595c106cf2905153df756e209cf51bd
Martichou <m@rtin.fyi>
2022-06-08 22:45:33 +0200

    new: reacting to status <3

--- src/views/NewServer.vue
+++ src/views/NewServer.vue
@@ -43,12 +43,13 @@
 			<div class="flex flex-col xl:flex-row xl:space-x-20 space-y-8 xl:space-y-0">
 				<div class="flex-2 mockup-code">
 					<pre data-prefix="1"><code>curl https://speculare.cloud/some_random_script.sh | bash</code></pre>
-					<pre data-prefix="3" class="bg-success text-success-content"><code>Success!</code></pre>
+					<pre v-if="host_uuid === null" data-prefix="2" class="bg-warning text-warning-content"><code>Waiting for data...</code></pre>
+					<pre v-if="host_uuid !== null" data-prefix="2" class="bg-success text-success-content"><code>Success!</code></pre>
 				</div>
 				<div class="flex-1 prose-sm xl:flex flex-col justify-center align-middle">
 					<h3>Download and setup the daemon</h3>
 					<p class="mt-3 text-[#c5c8cb] text-sm">
-						Use the one command installer and follow the prompt. Enter the KEY from the previous step
+						Use the one command installer and follow the prompt. Enter the key from the previous step
 						when asked.
 					</p>
 				</div>
@@ -57,17 +58,19 @@
 				finally
 			</div>
 			<div class="max-w-lg mx-auto flex flex-col justify-center gap-6">
-				<div class="prose-sm bg-base-300 rounded-lg shadow px-4 py-6">
-					<h3>Waiting for data</h3>
-					<p class="mt-3 text-[#c5c8cb] text-sm">
-						We're waiting for your server to send the first packet in order assert that everything
-						worked correctly.
+				<div class="prose-sm rounded-lg shadow px-4 py-6" :class="host_uuid === null ? 'bg-neutral bg-opacity-20' : 'bg-base-300'">
+					<h3 class="font-semibold" :class="host_uuid === null ? 'text-base-content text-opacity-20' : ''">
+						You're all set
+					</h3>
+					<p class="mt-3 text-sm" :class="host_uuid === null ? 'text-base-content text-opacity-20' : 'text-[#c5c8cb]'">
+						It's all good, your server is periodically sending metrics and you can now see
+						them in the dashboard.
 					</p>
 				</div>
 				<router-link
-					:to="{ name: 'DetailsServer', params: { uuid: 'item.uuid', hostname: 'item.hostname' } }"
-					class="btn btn-info !h-10 !min-h-[2.5rem] normal-case" key="add_server" disabled>
-					Goto server
+					:to="{ name: 'Servers' }" :class="host_uuid === null ? 'disabled' : ''"
+					class="btn btn-info !h-10 !min-h-[2.5rem] normal-case" key="add_server">
+					Goto servers
 				</router-link>
 			</div>
 		</div>
@@ -82,15 +85,25 @@ export default {
 
 	data () {
 		return {
+			interval: null,
 			secretKey: this.$route.params.secretKey ?? null,
+			host_uuid: null,
 		}
+	},
+
+	beforeUnmount: function() {
+		clearInterval(this.interval);
 	},
 
 	mounted: function () {
 		const vm = this
 
+		if (vm.interval === null) {
+			vm.interval = setInterval(vm.getKeyInfo, 5000);
+		}
 		nextTick(async () => {
 			if (vm.secretKey === null) await vm.generateKey();
+			else await vm.getKeyInfo();
 		})
 	},
 
@@ -101,6 +114,17 @@ export default {
 				.then((resp) => {
 					console.log(resp);
 					this.secretKey = resp.data.key;
+				}).catch((err) => {
+					// TODO - Handle errors
+					console.log(err);
+				});
+		},
+		getKeyInfo: async function() {
+			await this.$http.get(this.$authBase + "/api/key", { headers: { "SPTK": this.secretKey } })
+				.then((resp) => {
+					console.log(resp);
+					this.host_uuid = resp.data.host_uuid ?? null;
+					if (this.host_uuid !== null) clearInterval(this.interval);
 				}).catch((err) => {
 					// TODO - Handle errors
 					console.log(err);
