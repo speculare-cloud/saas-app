458714db7b0441eed5a08539ab1c94042c58f44e
Martichou <m@rtin.fyi>
2022-07-06 17:46:48 +0200

    tweak a bit the css

--- src/components/Graphs/Base/Skeleton.vue
+++ src/components/Graphs/Base/Skeleton.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="skeleton">
-		<div class="w-100 flex items-center justify-center text-xl text-gray-400" style="height: 258px">
+		<div class="w-100 flex items-center justify-center text-xl text-gray-400" style="height: 253.5px">
 			<h3>Loading</h3>
 		</div>
 	</div>
