54569e28540306b3caa6e23f97a1281ee33b991f
Martichou <m@rtin.fyi>
2023-05-20 00:52:11 +0200

    graph: disable points when not needed
    Signed-off-by: Martichou <m@rtin.fyi>

--- src/utils/graphsCharts.ts
+++ src/utils/graphsCharts.ts
@@ -4,14 +4,64 @@ const strokeColors = ['#EAB839', '#008080', '#DA70D6', '#EAB839']
 const fillColorsAlpha = ['#EAB8391A', '#0080801A', '#DA70D61A', '#EAB8391A']
 const fillColors = ['#EAB839', '#008080', '#DA70D6', '#EAB839']
 
+const pointsFilter = (u, seriesIdx, show, gaps) => {
+	const filtered = new Array<number>();
+	const series = u.series[seriesIdx];
+
+	if (!show && gaps && gaps.length) {
+		const [firstIdx, lastIdx] = series.idxs;
+		const xData = u.data[0];
+		const yData = u.data[seriesIdx];
+		const firstPos = Math.round(u.valToPos(xData[firstIdx], 'x', true));
+		const lastPos = Math.round(u.valToPos(xData[lastIdx], 'x', true));
+
+		if (gaps[0][0] === firstPos) {
+			filtered.push(firstIdx);
+		}
+
+		// show single points between consecutive gaps that share end/start
+		for (let i = 0; i < gaps.length; i++) {
+			const thisGap = gaps[i];
+			const nextGap = gaps[i + 1];
+
+			if (nextGap && thisGap[1] === nextGap[0]) {
+				// approx when data density is > 1pt/px, since gap start/end pixels are rounded
+				let approxIdx = u.posToIdx(thisGap[1], true);
+
+				if (yData[approxIdx] == null) {
+					// scan left/right alternating to find closest index with non-null value
+					for (let j = 1; j < 100; j++) {
+						if (yData[approxIdx + j] != null) {
+							approxIdx += j;
+							break;
+						}
+						if (yData[approxIdx - j] != null) {
+							approxIdx -= j;
+							break;
+						}
+					}
+				}
+
+				filtered.push(approxIdx);
+			}
+		}
+
+		if (gaps[gaps.length - 1][1] === lastPos) {
+			filtered.push(lastIdx);
+		}
+	}
+
+	return filtered.length ? filtered : null;
+};
+
 // TODO - Filter out point from rendering depending on
 // threshold and value (if prev and next is null, draw, otherwise no)
 function serie (idx: number, alpha = true) {
 	return {
 		value: (_, v) => intValueOrTilde(v),
 		points: {
-			show: true,
-			size: 3,
+			filter: pointsFilter,
+			show: false,
 		},
 		width: Math.min(Math.max(2 / devicePixelRatio, 1.25), 2),
 		stroke: strokeColors[idx],
